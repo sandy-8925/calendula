22c58cea0cf2265d9f5ef33619a31e0b9f1f3316
Sandeep Raghuraman <sandy.8925@gmail.com>
2025-04-15 14:10:10 +0530

    Code cleanup

--- Calendula/src/main/java/es/usc/citius/servando/calendula/util/ScreenUtils.java
+++ Calendula/src/main/java/es/usc/citius/servando/calendula/util/ScreenUtils.java
@@ -168,13 +168,10 @@ public class ScreenUtils {
     }
 
     public static void setStatusBarColor(Activity activity, @ColorInt int color) {
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = activity.getWindow();
-            window.setStatusBarColor(color);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-            setWindowFlag(activity, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
-        }
+        Window window = activity.getWindow();
+        window.setStatusBarColor(color);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        setWindowFlag(activity, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
     }
 
     public static void setWindowFlag(Activity activity, final int bits, boolean on) {
